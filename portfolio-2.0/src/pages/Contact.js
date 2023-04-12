f04e1d86dd5f56c68ded6981114bf28849649a7e
Yankee Knight <116320996+YankeeKnight@users.noreply.github.com>
2023-04-11 21:48:27 -0400

    setting up form

--- portfolio-2.0/src/pages/Contact.js
+++ portfolio-2.0/src/pages/Contact.js
@@ -7,24 +7,24 @@ function Contact() {
             <h2 className="text-white mb-4 headingCon">Reach Out</h2>
             <div className="container text-white py-4">
 
-                <form id="contactForm">
+                <form id="contactForm" action="https://getform.io/f/6bd6ad41-3a0f-4408-8f25-5d369abfc41a" method="POST">
 
 
                     <div className="mb-3">
                         <label className="form-label" for="name">Name</label>
-                        <input className="form-control" id="name" type="text" placeholder="Name" />
+                        <input className="form-control" id="name" type="text" placeholder="Name" name="name" />
                     </div>
 
 
                     <div className="mb-3">
                         <label className="form-label" for="emailAddress">Email Address</label>
-                        <input className="form-control" id="emailAddress" type="email" placeholder="Email Address" />
+                        <input className="form-control" id="emailAddress" type="email" placeholder="Email Address" name="email" />
                     </div>
 
 
                     <div className="mb-3">
                         <label className="form-label" for="message">Message</label>
-                        <textarea className="form-control" id="message" type="text" placeholder="Message"></textarea>
+                        <textarea className="form-control" id="message" type="text" placeholder="Message" name="message"></textarea>
                     </div>
 
 
